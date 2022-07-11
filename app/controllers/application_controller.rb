4cbe48382aefedb89ea24fadc52844142faa3cff
Bach Le <lehoangbach7802@gmail.com>
2022-07-11 16:50:37 -0400

    commit tree 128480cb0c86c8b0e37ba723c4dcd475c5fc5d1e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
+set :default_content_type, 'application/json'
 
-  # add routes
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
+  get "/baked_goods" do
+    baked_goods = BakedGood.all
+    baked_goods.to_json
+  end
+
+  get "/bakeries/:id" do
+    cake = Bakery.find(params[:id])
+    cake.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get "/baked_goods/most_expensive" do
+    baked_goods = BakedGood.most_expensive
+    baked_goods.to_json
+  end
 end
